7aad53c4216deb9e91dbe872a776c6c605f75483
ma21alsaeed <mahmoud.2.21.ma@gmail.com>
2024-03-27 23:32:54 +0200

    add logo

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const path = require('path');
 
 const app = express();
 const port = 3000;
+app.use(express.static('public'));
+app.use(express.static('templates'));
 
 // Middleware to parse JSON bodies
 app.use(express.json());
