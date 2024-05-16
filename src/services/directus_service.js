60eb15d85bf8f9c46b9fe1500ed55e0f674793e7
ma21alsaeed <mahmoud.2.21.ma@gmail.com>
2024-05-16 16:46:41 +0300

    fix login

--- src/services/directus_service.js
+++ src/services/directus_service.js
@@ -1,25 +1,36 @@
-const { createDirectus, rest, uploadFiles } = require('@directus/sdk');
+const { createDirectus, rest, uploadFiles, authentication } = require('@directus/sdk');
+
+let client;
+let token;
+
+const login = async () => {
+    client = createDirectus(process.env.DIRECTUS_ENDPOINT).with(authentication());
+    const response = await client.login( process.env.DIRECTUS_EMAIL,process.env.DIRECTUS_PASSWORD);
+    token = response.data.access_token;
+};
 
 const uploadPdf = async (pdfBuffer, quoteNo) => {
-    const directus = createDirectus(process.env.DIRECTUS_ENDPOINT);
-    const client = directus.with(rest());
-
-    // Login with your Directus username and password
-    const credentials = {
-        email: process.env.DIRECTUS_EMAIL,
-        password: process.env.DIRECTUS_PASSWORD
+    if (!token) {
+        await login();
     }
-    const response = await directus.auth.login(credentials);
-    const token = response.data.access_token;
 
-    // Use the token for subsequent requests
-    client.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    client = client.with(rest(token));
 
     const formData = new FormData();
     formData.append('file', new Blob([pdfBuffer], { type: 'application/pdf' }), `${quoteNo}.pdf`);
     formData.append('title', quoteNo);
 
-    return await client.request(uploadFiles(formData));
+    try {
+        return await client.request(uploadFiles(formData));
+    } catch (error) {
+        if (error.response && error.response.status === 401) {
+            // Token expired, login again
+            await login();
+            return await client.request(uploadFiles(formData));
+        } else {
+            throw error;
+        }
+    }
 };
 
 module.exports = {
