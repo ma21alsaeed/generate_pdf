36abf756512b559b74da391c70e5b1c161deb69c
ma21alsaeed <mahmoud.2.21.ma@gmail.com>
2024-05-16 16:26:42 +0300

    add enviroment variables

--- src/services/directus_service.js
+++ src/services/directus_service.js
@@ -1,7 +1,19 @@
 const { createDirectus, rest, uploadFiles } = require('@directus/sdk');
 
 const uploadPdf = async (pdfBuffer, quoteNo) => {
-    const client = createDirectus('http://207.244.242.3:8055').with(rest());
+    const directus = createDirectus(process.env.DIRECTUS_ENDPOINT);
+    const client = directus.with(rest());
+
+    // Login with your Directus username and password
+    const credentials = {
+        email: process.env.DIRECTUS_EMAIL,
+        password: process.env.DIRECTUS_PASSWORD
+    }
+    const response = await directus.auth.login(credentials);
+    const token = response.data.access_token;
+
+    // Use the token for subsequent requests
+    client.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
     const formData = new FormData();
     formData.append('file', new Blob([pdfBuffer], { type: 'application/pdf' }), `${quoteNo}.pdf`);
