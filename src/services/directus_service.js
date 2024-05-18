25275552bc28f484d2e714ea9acf10706d966ed9
ma21alsaeed <mahmoud.2.21.ma@gmail.com>
2024-05-18 16:47:16 +0300

    fix login

--- src/services/directus_service.js
+++ src/services/directus_service.js
@@ -7,12 +7,14 @@ const endpoint = process.env.DIRECTUS_ENDPOINT;
 
 const client = createDirectus(endpoint).with(authentication());;
 
-
+console.log(email);
+console.log(password);
+console.log(endpoint);
 const login = async () => {
   try {
     const response = await client.login({
-      email: email,
-      password: password,
+      email: email.toString(),
+      password: password.toString(),
     });
     console.log('Access token:', response.access_token);
   } catch (error) {
