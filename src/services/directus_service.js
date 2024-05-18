69f1d2950999d48aec5fe6715daba75f42b7da62
ma21alsaeed <mahmoud.2.21.ma@gmail.com>
2024-05-18 16:27:51 +0300

    fix login

--- src/services/directus_service.js
+++ src/services/directus_service.js
@@ -1,21 +1,25 @@
-import { createDirectus, authentication, login } from '@directus/sdk';
+
+const { createDirectus, authentication } = require('@directus/sdk');
+
 const email = process.env.DIRECTUS_EMAIL;
 const password = process.env.DIRECTUS_PASSWORD;
 const endpoint = process.env.DIRECTUS_ENDPOINT;
-const directus = createDirectus(endpoint);
 
+const directus = createDirectus(endpoint);
 const auth = authentication(directus);
+
 const login = async () => {
-try {
-  const response = await auth.login({
-    email: email,
-    password: password,
-  });
-  console.log('Access token:', response.access_token);
-} catch (error) {
-  console.error('Error:', error);
-}
+  try {
+    const response = await auth.login({
+      email: email,
+      password: password,
+    });
+    console.log('Access token:', response.access_token);
+  } catch (error) {
+    console.error('Error:', error);
+  }
 };
+
 module.exports = {
-    login,
+  login,
 };
