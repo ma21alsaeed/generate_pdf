2da78cf6cc0aa56a3cd5d631ec4e9d0c03b8e1dd
ma21alsaeed <mahmoud.2.21.ma@gmail.com>
2024-05-18 20:34:12 +0300

    fix login

--- src/services/directus_service.js
+++ src/services/directus_service.js
@@ -1,27 +1,52 @@
-
-const { createDirectus, authentication } = require('@directus/sdk');
+const { createDirectus, authentication, rest, uploadFiles } = require('@directus/sdk');
 
 const email = process.env.DIRECTUS_EMAIL;
 const password = process.env.DIRECTUS_PASSWORD;
 const endpoint = process.env.DIRECTUS_ENDPOINT;
 
-const client = createDirectus(endpoint).with(authentication());;
+const client = createDirectus(endpoint).with(authentication());
 
-console.log(email);
-console.log(password);
-console.log(endpoint);
+let access_token;
 const login = async () => {
   try {
     const response = await client.login(
         email.toString(),
-     password.toString(),
+        password.toString(),
     );
-    console.log('Access token:', response.access_token);
+    access_token = response.access_token;
+    await client.setToken(access_token);
   } catch (error) {
     console.error('Error:', error);
   }
 };
 
+const validateToken = async () => {
+  if (!access_token) {
+    await login();
+  } else {
+    try {
+      // Assuming there's a method to validate the token
+      const isValid = await client.validateToken(access_token);
+      if (!isValid) {
+        await login();
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      await login();
+    }
+  }
+};
+
+const uploadPdf = async (pdfBuffer, quoteNo) => {
+    await validateToken();
+    
+    const formData = new FormData();
+    formData.append('file', new Blob([pdfBuffer], { type: 'application/pdf' }), `${quoteNo}.pdf`);
+    formData.append('title', quoteNo);
+
+    return await client.request(uploadFiles(formData));
+};
+
 module.exports = {
-  login,
+  uploadPdf
 };
